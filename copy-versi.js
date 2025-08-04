59f9a7407ec732b80e1e315fd30d36db04d8463d
Natasya Tanjaya <45988890+natasyatanjaya2@users.noreply.github.com>
2025-08-05 01:53:39 +0800

    Create copy-versi.js

---
+++ copy-versi.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// 🔹 Endpoint versi.json
+app.get('/versi.json', (req, res) => {
+  res.json({
+    versi: "1.4",
+    url: "https://github.com/natasyatanjaya2/my-updater-api/raw/refs/heads/main/releases/latest/download/ProgramToko.exe",
+    files: [
+      { 
+        nama: "filetambahan.dll", 
+        url: "https://raw.githubusercontent.com/natasyatanjaya2/my-updater-api/main/filetambahan.dll" 
+      },
+      { 
+        nama: "filetambahan2.txt", 
+        url: "https://raw.githubusercontent.com/natasyatanjaya2/my-updater-api/main/filetambahan2.txt" 
+      }
+    ]
+  });
+});
+
+// Jalankan server
+app.listen(PORT, () => console.log(`✅ Server berjalan di ${PORT}`));
