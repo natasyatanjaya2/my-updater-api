3db24eb8997350cacdd7d89ac17a5bbda8bb7567
Natasya Tanjaya <45988890+natasyatanjaya2@users.noreply.github.com>
2025-08-05 01:49:07 +0800

    Update versi.js

--- versi.js
+++ versi.js
@@ -5,24 +5,21 @@ const PORT = process.env.PORT || 3000;
 
 // 🔹 Endpoint versi.json
 app.get('/versi.json', (req, res) => {
-  const baseUrl = `${req.protocol}://${req.get('host')}`; // URL otomatis Railway
-
   res.json({
     versi: "1.4",
     url: "https://github.com/natasyatanjaya2/my-updater-api/raw/refs/heads/main/releases/latest/download/ProgramToko.exe",
     files: [
-      { path: "https://raw.githubusercontent.com/natasyatanjaya2/my-updater-api/main/filetambahan.dll" },
-      { path: "https://raw.githubusercontent.com/natasyatanjaya2/my-updater-api/main/filetambahan2.txt" }
+      { 
+        nama: "filetambahan.dll", 
+        url: "https://raw.githubusercontent.com/natasyatanjaya2/my-updater-api/main/filetambahan.dll" 
+      },
+      { 
+        nama: "filetambahan2.txt", 
+        url: "https://raw.githubusercontent.com/natasyatanjaya2/my-updater-api/main/filetambahan2.txt" 
+      }
     ]
   });
 });
 
 // Jalankan server
 app.listen(PORT, () => console.log(`✅ Server berjalan di ${PORT}`));
-
-
-
-
-
-
-
