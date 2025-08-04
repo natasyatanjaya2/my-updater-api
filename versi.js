caa3e5bf439d8b14d6a5448fc13f96fe298ca31e
Natasya Tanjaya <45988890+natasyatanjaya2@users.noreply.github.com>
2025-08-04 21:49:41 +0800

    Update versi.js

--- versi.js
+++ versi.js
@@ -4,10 +4,14 @@ const PORT = process.env.PORT || 3000;
 
 app.get('/versi.json', (req, res) => {
   res.json({
-    versi: "1.2",
-    url: "https://link-download.exe",
-    files: []
+    versi: "1.3",
+    url: "https://raw.githubusercontent.com/natasyatanjaya2/my-updater-api/main/public/ProgramToko.exe",
+    files: [
+      { path: "file_tambahan_1.dll" },
+      { path: "folder/subfolder/file_tambahan_2.txt" }
+    ]
   });
 });
+
 
 app.listen(PORT, () => console.log(`Server berjalan di http://localhost:${PORT}`));
