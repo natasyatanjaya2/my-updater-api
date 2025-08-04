7e152fc3c012b24dab569d994b5622a7e80c8648
Natasya Tanjaya <45988890+natasyatanjaya2@users.noreply.github.com>
2025-08-04 22:48:33 +0800

    Update versi.js

--- versi.js
+++ versi.js
@@ -3,9 +3,6 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// 🔹 Folder tempat file update disimpan (public)
-app.use(express.static(path.join(__dirname, 'public')));
-
 // 🔹 Endpoint versi.json
 app.get('/versi.json', (req, res) => {
   const baseUrl = `${req.protocol}://${req.get('host')}`; // URL otomatis Railway
@@ -22,5 +19,6 @@ app.get('/versi.json', (req, res) => {
 
 // Jalankan server
 app.listen(PORT, () => console.log(`✅ Server berjalan di ${PORT}`));
+
 
 
