c12c05f15cdc3cc7b0605fbca59703354372a953
Natasya Tanjaya <45988890+natasyatanjaya2@users.noreply.github.com>
2025-12-06 12:06:34 +0700

    Update versi.js

--- versi.js
+++ versi.js
@@ -6,20 +6,15 @@ const PORT = process.env.PORT || 3000;
 // 🔹 Endpoint versi.json
 app.get('/versi.json', (req, res) => {
   res.json({
-    versi: "1.8",
-    url: "",
-    files: [
-      {
-        "nama": "bot-wa.zip",
-        "url": "https://github.com/natasyatanjaya2/my-updater-api/raw/refs/heads/main/bot-wa.zip",
-        "extractTo": "bot-wa"
-      }
-    ]
+    versi: "1.0",
+    url: "https://github.com/natasyatanjaya2/my-updater-api/raw/refs/heads/main/releases/latest/download/ProgramToko.exe",
+    files: []
   });
 });
 
 // Jalankan server
 app.listen(PORT, () => console.log(`✅ Server berjalan di ${PORT}`));
+
 
 
 
